cae0d06923a57fc0793203c2c87a32c3fb856d82
gs2usr <jglarios1@gmail.com>
2013-06-29 05:49:40 -0700

    QS: Fliptile animation [1/2]
    Credits to TeamBAMF for the original work
    
    Change-Id: I8382e110dd218d625f38beaa7592172905e08f3e
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/FChargeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/ThemeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/TorchTile.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/BatterySaverTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/CompassTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/OnTheGoTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/ThemeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/TorchTile.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/HoverTile.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4050,6 +4050,13 @@ public final class Settings {
         public static final String HOVER_STATE = "hover_state";
 
         /**
+         * Animate-flip Quick Settings Panel Tiles on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
