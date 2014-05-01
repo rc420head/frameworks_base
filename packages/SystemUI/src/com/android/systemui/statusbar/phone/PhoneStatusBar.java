52a5659ee0120b9e594aa120728308875fbb21bc
errantzebra845 <errantzebra845@gmail.com>
2014-05-01 17:24:30 -0400

    Density on the go 2/2
    Change-Id: I7d935c568f75f18df8802ea8c71934bff413e127

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -213,6 +213,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     Point mCurrentDisplaySize = new Point();
     int mCurrUiThemeMode;
     int mCurrOrientation;
+    int mCurrentDensity;
     private float mHeadsUpVerticalOffset;
     private int[] mPilePosition = new int[2];
 
@@ -947,6 +948,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDisplaySize();
 
         mCurrUiThemeMode = mContext.getResources().getConfiguration().uiThemeMode;
+        mCurrentDensity = mContext.getResources().getConfiguration().densityDpi;
 
         mLocationController = new LocationController(mContext); // will post a notification
         mBatteryController = new BatteryController(mContext);
@@ -3975,6 +3977,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (uiThemeMode != mCurrUiThemeMode) {
             mCurrUiThemeMode = uiThemeMode;
             recreateStatusBar(false);
+            return;
+        }
+
+        // detect density change
+        int density = res.getConfiguration().densityDpi;
+        if (density != mCurrentDensity) {
+            mCurrentDensity = density;
+            recreateStatusBar(true);
+            recreatePie(isPieEnabled());
             return;
         }
 
